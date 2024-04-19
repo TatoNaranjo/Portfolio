6e9587808ce323db88895ea625f6f29698009bf3
TatoNaranjo <naranjosa2004@gmail.com>
2024-04-18 22:09:15 -0500

    first commit

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'TatoNaranjo';
+export const SITE_DESCRIPTION = 'Este es el portafolio personal de Santiago Naranjo Herrera, estudiante de ingeniería de sistemas y computación, y apasionado por el autoaprendizaje.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
